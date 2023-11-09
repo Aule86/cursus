ec1478aa2de5a97a601274758683b9e8e1153490
Asier <129412105+Aule86@users.noreply.github.com>
2023-11-09 15:35:13 +0100

    actualización gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aszamora <aszamora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 12:17:57 by aszamora          #+#    #+#             */
-/*   Updated: 2023/11/07 14:25:49 by aszamora         ###   ########.fr       */
+/*   Updated: 2023/11/09 13:54:49 by aszamora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,65 @@ int	ft_find_nl(char *static_buff)
 	if (!static_buff)
 		return (-1);
 	i = 0;
-	while (static_buff[i] != '\0' && static_buff[i] != '\n')
+	while (static_buff[i] != '\n' && static_buff[i] != '\0')
 		i++;
 	if (static_buff[i] == '\0')
 		return (-1);
 	return (i);
+}
+
+char	*ft_finish_line(char *static_buff, int i)
+{
+	char	*line;
+	int		y;
+
+	line = (char *)malloc((i + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	y = 0;
+	while (static_buff[y] != '\n')
+	{
+		line[y] = static_buff[y];
+		y++;
+	}
+	line[y] = '\n';
+	line [y + 1] = '\0';
+	return (line);
+}
+
+char	*ft_line(char *static_buff)
+{
+	char	*line;
+	int		i;
+
+	if (ft_strlen(static_buff) <= 0)
+		return (NULL);
+	i = ft_find_nl(static_buff);
+	if (i == -1 || i == ((int)ft_strlen(static_buff) - 1))
+	{
+		line = ft_strdup(static_buff);
+		return (line);
+	}
+	line = ft_finish_line(static_buff, i);
+	return (line);
+}
+
+char	*ft_static_buff(char *static_buff)
+{
+	int				i;
+	unsigned int	len;
+
+	if (!static_buff)
+		return (NULL);
+	len = ft_strlen(static_buff);
+	i = ft_find_nl(static_buff);
+	if (i == -1 || ((len - i) == 1))
+	{
+		free(static_buff);
+		return (NULL);
+	}
+	static_buff = ft_substr(static_buff, (i + 1), (len - i - 1));
+	return (static_buff);
 }
 
 char	*get_next_line(int fd)
@@ -41,7 +95,7 @@ char	*get_next_line(int fd)
 	bytes = 1;
 	while (ft_find_nl(static_buff) == -1)
 	{
-		bytes = read (fd, buffer, BUFFER_SIZE);
+		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes <= 0)
 			break ;
 		buffer[bytes] = '\0';
@@ -50,4 +104,7 @@ char	*get_next_line(int fd)
 	free(buffer);
 	if (bytes == -1)
 		return (NULL);
+	line = ft_line(static_buff);
+	static_buff = ft_static_buff(static_buff);
+	return (line);
 }
