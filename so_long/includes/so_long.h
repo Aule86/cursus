c7433715baac865150f6b14d720db34190c31586
Aule86 <asierz86@gmail.com>
2023-11-29 12:22:15 +0100

    so_long primer commit

---
+++ so_long/includes/so_long.h
@@ -0,0 +1,12 @@
+
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+# include <limits.h>
+# include "../mlx/mlax.h"
