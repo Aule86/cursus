f09625091b17732c8c7396610801049ea8b53c0b
Aule86 <asierz86@gmail.com>
2023-11-29 12:22:15 +0100

    so_long primer commit

---
+++ so_long/includes/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aszamora <aszamora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 15:00:40 by aszamora          #+#    #+#             */
+/*   Updated: 2023/11/27 11:06:03 by aszamora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1 
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_static_buff(char *static_buff);
+char	*ft_line(char *static_buff);
+char	*ft_finish_line(char *static_buff, int i);
+int		ft_find_nl(char *static_buff);
+void	read_error(char **static_buff, char **buffer, int bytes);
+
+#endif
