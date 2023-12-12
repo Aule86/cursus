cf37be94d5ea67f3fb618a5cb4410429128a2408
Asier Zamorano Villalba <asierz86@gmail.com>
2023-12-12 16:23:45 +0100

    actualizacion

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,12 +6,13 @@
 /*   By: aszamora <aszamora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 11:58:53 by aszamora          #+#    #+#             */
-/*   Updated: 2023/11/30 12:00:37 by aszamora         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:27:13 by aszamora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -19,4 +20,24 @@
 # include <string.h>
 # include <errno.h>
 # include <limits.h>
-# include "../includes/mlax.h"
+# include "../mlx/mlx.h"
+# include "get_next_line.h"
+
+typedef struct s_map
+{
+	char	**map;
+	int		fd;
+	int		width;
+	int		height;
+	int		cols;
+	int		rows;
+	int		player;
+	int		collectables;
+	void	*mlx_connection;
+	void	*mlx_window;
+
+}				t_map;
+
+int	read_map(t_map *game, char **argv);
+
+#endif
