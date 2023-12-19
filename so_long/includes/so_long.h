f099b23b899e299a00726701262f319c3476da76
Asier Zamorano Villalba <asierz86@gmail.com>
2023-12-19 13:20:43 +0100

    actualizacion

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: aszamora <aszamora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 11:58:53 by aszamora          #+#    #+#             */
-/*   Updated: 2023/12/12 12:27:13 by aszamora         ###   ########.fr       */
+/*   Updated: 2023/12/14 13:41:33 by aszamora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,30 @@ typedef struct s_map
 	int		fd;
 	int		width;
 	int		height;
-	int		cols;
-	int		rows;
+	int		colums;
 	int		player;
+	int		enemies;
+	int		exit;
 	int		collectables;
 	void	*mlx_connection;
 	void	*mlx_window;
+	void	*winpointer;
+	void	*mlx_pointer;
 
 }				t_map;
 
-int	read_map(t_map *game, char **argv);
+int				main(int argc, char **argv);
+void			*ft_memset(void *b, int c, size_t len);
+int				quit_game(t_map *game);
+static void		check_walls(t_map *game);
+int				read_map(t_map *game, char **argv);
+static int		add_line(t_map *game, char *line);
+static int		map_width(char *str);
+void			check_errors(t_map *game);
+void			correct_player(t_map *game);
+static void		check_content(t_map *game, int height, int width);
+static void		check_walls(t_map *game);
+static int		verticalwall(t_map *game);
+static int		horizontalwall(t_map *game);
 
 #endif
