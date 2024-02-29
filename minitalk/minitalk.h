bb98eee3d243047e8f2f139a24da69ccbb34b5ae
Aule86 <asierz86@gmail.com>
2024-02-29 10:55:17 +0100

    primera subida

---
+++ minitalk/minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izperez <izperez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 13:17:09 by izperez           #+#    #+#             */
+/*   Updated: 2024/02/01 13:43:54 by izperez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "libft/libft.h"
+
+# define ERROR "\033[1;31,[Error]\033[0m\n"
+# define CONGRATS "\033[1;31,Congrats!\033[0m\n"
+
+#endif
