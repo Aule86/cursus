649808a5e39293ce48105e6301bf820a1022961c
Asier Zamorano Villalba <asierz86@gmail.com>
2024-03-05 11:16:18 +0100

    minitalk

---
+++ minitalk/ft_client.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_client.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aszamora <aszamora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 10:46:30 by aszamora          #+#    #+#             */
+/*   Updated: 2024/03/04 13:32:34 by aszamora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int				i;
+	unsigned int	result;
+	int				negative;
+
+	i = 0;
+	result = 0;
+	negative = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative = -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return ((int)result * negative);
+}
+
+void	text_binary(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(250);
+		bit++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (ac == 3)
+	{
+		pid = ft_atoi(av[1]);
+		while (av[2][i])
+		{
+			text_binary(pid, av[2][i]);
+			i++;
+		}
+		text_binary(pid, '\n');
+	}
+	write(1, "\n", 1);
+}
