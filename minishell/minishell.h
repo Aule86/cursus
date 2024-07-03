0c9deaf838548d1bf2582993f87d73d9eb093860
Asier Zamorano <aule86@MacBook-Air-de-Asier.local>
2024-07-03 14:17:15 +0200

    primer commit

---
+++ minishell/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aule86 <aule86@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 12:00:20 by aule86            #+#    #+#             */
+/*   Updated: 2024/07/03 13:26:54 by aule86           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include <signal.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+# define BUFFER_SIZE 1024
+
+void	read_command(char *buffer);
+char	**parse_command(char *buffer);
+void	execute_command(char **argv, char **envp);
+
+#endif
